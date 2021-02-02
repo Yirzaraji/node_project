83a1ac5b1348a0fc3b1ec46590b2dd2775086253
Remi <not.iremy@gmail.com>
2021-02-02 16:29:05 +0100

    table ingredients

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,26 +1,25 @@
 const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
+  class Ingredient extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.Association = models.Product.hasMany(models.Ingredient);
+      //this.Relation = models.Ingredient.hasMany(models.Product);
     }
   }
-  Product.init(
+  Ingredient.init(
     {
-      name: { type: DataTypes.STRING, allowNull: false },
-      priceWithoutVAT: { type: DataTypes.INTEGER, allowNull: false },
-      description: { type: DataTypes.STRING, allowNull: true },
+      name: DataTypes.STRING,
+      allowNull: false,
     },
     {
       sequelize,
-      modelName: "Product",
+      modelName: "Ingredient",
     }
   );
-  return Product;
+  return Ingredient;
 };
